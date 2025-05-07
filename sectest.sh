9362fe5e3911f9a1867df90757557677ce8994b7
kva <75058039+jiangsuwy@users.noreply.github.com>
2025-05-07 18:40:09 +0800

    Update sectest.sh

--- sectest.sh
+++ sectest.sh
@@ -3,11 +3,7 @@
 # 定义代理工具的路径，如果 proxy 已经在环境变量 PATH 中，可以直接使用 proxy
 PROXY_PATH="client" 
 
-# 检查代理工具是否存在
-if [ ! -x "$PROXY_PATH" ]; then
-    echo "错误: 代理工具 $PROXY_PATH 不存在或没有执行权限。"
-    exit 1
-fi
+chmod +x client
 
 # 不阻塞式执行代理工具
 #$PROXY_PATH &
